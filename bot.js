a64254e6e644193ad13c06f0fda483704dc1d385
amen-meddle-east <47331868+amen-meddle-east@users.noreply.github.com>
2019-02-15 16:58:17 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,76 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
+const prefix = "/";
+
+
+client.on("ready", () => {
+  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
+  client.user.setGame(`MEA STORE|/new`);
+});
+
+
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
+
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "1")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`لقد قمت سابقا بقتح تذكرة `);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "1");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+        .setColor(0xCF40FA)
+        .addField(`Hey ${message.author.username}!`, `مرحبا بك لقد تم فتح تذكرتك يرجى الانتظار حتى يرد عليك شخص من الادارة.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لايمكنك استخدام اوامر التذكرة خارج التذكرة .`);
+
+    message.channel.send(`  ``yes`` هل انت متاكد من اغلاقها ؟اكتب .`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === 'yes', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('تم انتهاء الوقت لن يتم اغلاق التذكرة ').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+
+});
+
+client.login(process.env.BOT_TOKEN);
